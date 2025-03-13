74da0d65678787277a60c84aac3c7e4a15c47b10
Lester Hedges <lester.hedges@gmail.com>
2025-03-13 20:22:40 +0000

    Add argument parser to demo.

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import argparse
+
 from loch import GCMCSampler
 
 import sire as sr
@@ -6,22 +8,53 @@ from sire.base import ProgressBar as _ProgressBar
 _ProgressBar.set_silent()
 del _ProgressBar
 
+parser = argparse.ArgumentParser("GCMC sampler demo")
+parser.add_argument(
+    "--cutoff-type",
+    type=str,
+    default="rf",
+    help="The non-bonded cutoff type",
+    choices=["rf", "pme"],
+    required=False,
+)
+parser.add_argument(
+    "--cutoff",
+    type=str,
+    default="10 A",
+    help="The non-bonded cutoff",
+    required=False,
+)
+parser.add_argument(
+    "--num-attempts",
+    type=int,
+    default=10000,
+    help="The number of GCMC insertion attempts",
+    required=False,
+)
+args = parser.parse_args()
+
 # Load the scytalone dehydratase system
 mols = sr.load("examples/scytalone-dehydratase/outputs/*7")
+
+# Store the reference selection.
+reference = "(residx 22 or residx 42) and (atomname OH)"
 
 # Create a GCMC sampler.
 sampler = GCMCSampler(
     mols,
-    "(residx 22 or residx 42) and (atomname OH)",
-    num_attempts=10000,
-    cutoff_type="rf",
+    reference,
+    num_attempts=args.num_attempts,
+    cutoff_type=args.cutoff_type,
+    cutoff=args.cutoff,
     log_level="debug",
 )
 
 # Create a dynamics object using the modified GCMC system.
 # This contains a number of ghost waters that can be used
 # for insertion moves.
-d = sampler.system().dynamics(cutoff_type="rf", pressure=None)
+d = sampler.system().dynamics(
+    cutoff_type=args.cutoff_type, cutoff=args.cutoff, pressure=None
+)
 
 # Run 100 dynamics cycles with a GCMC move after each cycle.
 for i in range(100):
@@ -40,3 +73,7 @@ for i in range(100):
 print(f"Accepted: {sampler.num_accepted()}")
 print(f"Insertions: {sampler.num_insertions()}")
 print(f"Deletions: {sampler.num_deletions()}")
+
+# Save the final configuration.
+mols = d.commit()
+sr.save(mols[f"mols within 10 of {reference}"], f"final_{args.cutoff_type}.pdb")
