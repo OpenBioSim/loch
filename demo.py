7d47365d2a31bd9cbb458de1a1b2e86907d284db
Lester Hedges <lester.hedges@gmail.com>
2025-03-13 19:31:18 +0000

    Add dynamics to demo script.

--- demo.py
+++ demo.py
@@ -22,21 +22,20 @@ sampler = GCMCSampler(
 # This contains a number of ghost waters that can be used
 # for insertion moves.
 d = sampler.system().dynamics(cutoff_type="rf", pressure=None)
-context = d.context()
 
 # Run 100 dynamics cycles with a GCMC move after each cycle.
 for i in range(100):
     print(f"Cycle {i}")
 
     # Run 1ps of dynamics.
-    #d.run("1ps", save_frequency=0)
+    d.run("1ps", save_frequency=0)
 
     # Perform a GCMC move.
-    context, move, accepted = sampler.move(context)
+    context, move, accepted = sampler.move(d.context())
 
     # If the move was accepted, update the dynamics object.
-    #if accepted:
-        #d._d._omm_mols = context
+    if accepted:
+        d._d._omm_mols = context
 
 print(f"Accepted: {sampler.num_accepted()}")
 print(f"Insertions: {sampler.num_insertions()}")
