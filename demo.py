59a8be3d7194414efba811670991a370b72aa297
Lester Hedges <lester.hedges@gmail.com>
2025-03-13 15:59:01 +0000

    Update demo with API changes.

--- demo.py
+++ demo.py
@@ -1,9 +1,15 @@
 from loch import GCMCSampler
 
 import sire as sr
+from sire.base import ProgressBar as _ProgressBar
 
+_ProgressBar.set_silent()
+del _ProgressBar
+
+# Load the scytalone dehydratase system
 mols = sr.load("examples/scytalone-dehydratase/outputs/*7")
 
+# Create a GCMC sampler.
 sampler = GCMCSampler(
     mols,
     "(residx 22 or residx 42) and (atomname OH)",
@@ -12,14 +18,26 @@ sampler = GCMCSampler(
     log_level="debug",
 )
 
+# Create a dynamics object using the modified GCMC system.
+# This contains a number of ghost waters that can be used
+# for insertion moves.
 d = sampler.system().dynamics(cutoff_type="rf", pressure=None)
 context = d.context()
 
-num_accepted = 0
+# Run 100 dynamics cycles with a GCMC move after each cycle.
 for i in range(100):
-    context, _, accepted = sampler.move(context)
+    print(f"Cycle {i}")
 
-print()
+    # Run 1ps of dynamics.
+    #d.run("1ps", save_frequency=0)
+
+    # Perform a GCMC move.
+    context, move, accepted = sampler.move(context)
+
+    # If the move was accepted, update the dynamics object.
+    #if accepted:
+        #d._d._omm_mols = context
+
 print(f"Accepted: {sampler.num_accepted()}")
 print(f"Insertions: {sampler.num_insertions()}")
 print(f"Deletions: {sampler.num_deletions()}")
