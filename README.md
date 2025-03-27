6457575e341a283b1d21a8a69ce16b203200ba85
Lester Hedges <lester.hedges@gmail.com>
2025-03-27 10:47:33 +0000

    Rework examples.

--- README.md
+++ README.md
@@ -8,15 +8,27 @@ Prototype GCMC water sampling code.
 conda create -f environment.yml
 ```
 
-## Usage
+## Examples
 
-Run 100 1ps dynamics blocks for scytalone dehydratase, performing 10000
-GCMC trial moves (insertions or deletions) after each block:
+### Scytalone dehydratase
+
+Run 100 1ps dynamics blocks for scytalone dehydratase, performing GCMC trial
+moves after each block:
 
 ```bash
-python demo.py --cutoff-type rf
+cd examples/scytalone
+python sd.py --cutoff-type pme
 ```
 
 When complete, the script will write a PDB file containing all molecules
 within the GCMC sphere radius, e.g. `final_rf.pdb`. For comparison, the initial
 configuration is written to `initial.pdb`.
+
+### Bulk water
+
+Run 10ns of bulk water sampling, with GCMC moves every 1ps:
+
+```bash
+cd examples/water
+python water.py --cutoff-type pme
+```
--- examples/water/water.py
+++ examples/water/water.py
@@ -1,4 +1,5 @@
 import argparse
+import openmm
 
 from time import time
 
@@ -20,20 +21,6 @@ parser.add_argument(
     help="The non-bonded cutoff",
     type=str,
     default="10 A",
-    required=False,
-)
-parser.add_argument(
-    "--radius",
-    help="The radius of the GCMC sphere",
-    type=str,
-    default="4 A",
-    required=False,
-)
-parser.add_argument(
-    "--ligand",
-    help="The ligand index",
-    type=int,
-    default=None,
     required=False,
 )
 parser.add_argument(
@@ -61,38 +48,34 @@ parser.add_argument(
     "--num-cycles",
     help="The number of dynamics cycles",
     type=int,
-    default=100,
+    default=10000,
     required=False,
 )
 args = parser.parse_args()
 
-# Load the scytalone dehydratase system
-if args.ligand is None:
-    mols = sr.load(f"examples/scytalone-dehydratase/outputs/*7")
-else:
-    mols = sr.load(f"examples/scytalone-dehydratase/outputs/lig{args.ligand}/*7")
+# Load the water box.
+mols = sr.load_test_files("water_box.prm7", "water_box.rst7")
 
-# Create the PDB suffix.
-suffix = f"_lig{args.ligand}" if args.ligand is not None else ""
+# Store the box information.
+space = mols.property("space")
 
-# Store the reference selection.
-reference = "(residx 22 or residx 42) and (atomname OH)"
+# Store the volume.
+volume = space.volume()
 
-# Save the initial configuration.
-sr.save(
-    mols[f"mols within {args.radius} of {reference}"],
-    f"initial{suffix}.pdb",
-)
+# Store Avagadro's number.
+NA = openmm.unit.AVOGADRO_CONSTANT_NA._value
 
 # Create a GCMC sampler.
 sampler = GCMCSampler(
     mols,
-    reference=reference,
+    reference=None,
     num_attempts=args.num_attempts,
     cutoff_type=args.cutoff_type,
     cutoff=args.cutoff,
-    radius=args.radius,
+    excess_chemical_potential="-6.16 kcal/mol",
+    standard_volume="30.543 A^3",
     temperature=args.temperature,
+    max_gcmc_waters=100,
     log_level="debug",
 )
 
@@ -111,18 +94,14 @@ d = sampler.system().dynamics(
 # Get the context.
 context = d.context()
 
+# Store the mass of a water molecule.
+mass = mols[0].mass()
+
+num_waters = mols.num_molecules()
+
 # Run dynamics cycles with a GCMC move after each.
 total = 0
 for i in range(args.num_cycles):
-    print(
-        f"Cycle {i}, N = {sampler.num_waters()}, "
-        f"insertions = {sampler.num_insertions()}, "
-        f"deletions = {sampler.num_deletions()}"
-    )
-    print(
-        f"Current potential energy: {d.current_potential_energy().value():.3f} kcal/mol"
-    )
-
     # Run 1ps of dynamics.
     d.run(args.cycle_time, save_frequency=0, energy_frequency=0, frame_frequency=0)
 
@@ -137,15 +116,22 @@ for i in range(args.num_cycles):
     if accepted:
         d._d._omm_mols = context
 
+    print(
+        f"Cycle {i}, N = {sampler.num_waters()}, "
+        f"insertions = {sampler.num_insertions()}, "
+        f"deletions = {sampler.num_deletions()}"
+    )
+    print(
+        f"Current potential energy: {d.current_potential_energy().value():.3f} kcal/mol"
+    )
+
+    # Work out the current density in g/mL.
+    total_mass = sampler.num_waters() * mass
+    density = ((total_mass * sr.units.mole) / (volume * NA)).to("g/centimeter^3")
+    print(f"volume: {volume.value():.5f} A^3, density: {density:.5f} g/mL")
+
 print(f"Insertions: {sampler.num_insertions()}")
 print(f"Deletions: {sampler.num_deletions()}")
 print(f"Move acceptance probability: {sampler.move_acceptance_probability():.4f}")
 print(f"Attempt acceptance probability: {sampler.attempt_acceptance_probability():.4f}")
 print(f"Average time: {1000*total / (args.num_cycles - 1):.3f} ms")
-
-# Save the final configuration.
-mols = d.commit()
-sr.save(
-    mols[f"mols within {sampler._radius.value()} of {reference}"],
-    f"final_{args.cutoff_type}{suffix}.pdb",
-)
