f92eff661da0fd438a1ce01cde36586aade34923
Lester Hedges <lester.hedges@gmail.com>
2025-04-29 11:13:35 +0100

    Minimise after swapping state between OpenMM contexts.

--- examples/bpti/bpti.py
+++ examples/bpti/bpti.py
@@ -137,10 +137,11 @@ d_npt.run("500 ps", save_frequency=0, energy_frequency=0, frame_frequency=0)
 # Get the updated Sire system from the dynamics object.
 mols = d_npt.commit()
 
-# Copy the state between the two contexts.
+# Copy the state between the two contexts and minmise the system.
 d._d._omm_mols.setState(
     d_npt._d._omm_mols.getState(getPositions=True, getVelocities=True)
 )
+d.minimise()
 
 # Update the box information in the GCMC sampler.
 sampler.set_box(mols)
